a60c31ca49008dcdfd9460b08aa508dbac8f3063
Sara_xy66@student.itstep.org <Sara_xy66@student.itstep.org>
2022-11-26 22:10:32 +0200

    Добавьте файлы проекта.

---
+++ RickMortyEkzamen_Json/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace RickMortyEkzamen_Json
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using (var client = new HttpClient())
+            {
+                string jsonData = client.GetStringAsync("https://rickandmortyapi.com/api/character").Result;
+
+                RickAndMortyAPI api = JsonConvert.DeserializeObject<RickAndMortyAPI>(jsonData);
+
+                foreach (var character in api.results)
+                {
+                    lstCharacters.Items.Add(character);
+                }
+            }
+
+        }
+
+        private void lstCharacters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedCharacter = (Character)lstCharacters.SelectedItem;
+            imgCharacter.Source = new BitmapImage(new Uri(selectedCharacter.image));
+        }
+    }
+}
---
+++ RickMortyEkzamen_Json/RickAndMortyAPI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace RickMortyEkzamen_Json
+{
+    public class RickAndMortyAPI
+    {
+
+        public Info info { get; set; }
+
+        public List<Character> results { get; set; }
+
+    }
+
+    public class Character
+    {
+        public string name { get; set; }
+
+        public string image { get; set; }
+
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return $"{name}";
+        }
+    }
+
+    public class Info
+    {
+        public int count { get; set; }
+        public int pages { get; set; }
+
+        public string next { get; set; }
+    }
+}
